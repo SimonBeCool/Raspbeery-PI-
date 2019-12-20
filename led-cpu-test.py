5d948e5c148f46aae36e06c9b2815ae7814aaca2
サイモン <52166507+SimonBeCool@users.noreply.github.com>
2019-12-20 16:54:49 +0100

    Add math libary to round off cpu temperature

--- led-cpu-test.py
+++ led-cpu-test.py
@@ -1,5 +1,6 @@
 from gpiozero import LED, CPUTemperature
 from time import sleep
+import math
 
 # LED variable configuration
 is_normal = LED(17)
@@ -10,10 +11,12 @@ cpu = CPUTemperature()
 
 if cpu.temperature < 60:
  is_normal.on()
+ print("CPU Temperature is " + math.floor(cpu.temperature) + " °C")
  sleep(5)
  is_normal.off()
 
 else: 
   warning.on()
+  print("CPU Temperature is " + math.floor(cpu.temperature) + " °C")
   sleep(5)
   warning.off()
